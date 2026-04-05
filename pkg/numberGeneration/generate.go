f4d6a5dd6ec47713dec27a4df8c58bd9abc24583
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 12:56:33 +0500

    Auto commit: 2026-04-05 12:56:33

--- pkg/numberGeneration/generate.go
+++ pkg/numberGeneration/generate.go
@@ -3,22 +3,3 @@ package numbergeneration
 import "gorm.io/gorm"
 
 
-func getMaxCounter(db *gorm.DB, base string) int {
-    var lastSlug string
-    db.Model(&models.Post{}).
-        Select("slug").
-        Where("slug LIKE ?", base+"%").
-        Order("id DESC").
-        Limit(1).
-        Scan(&lastSlug)
-
-    // salom-02 → 2 ni ajratish
-    parts := strings.Split(lastSlug, "-")
-    if len(parts) > 1 {
-        n, err := strconv.Atoi(parts[len(parts)-1])
-        if err == nil {
-            return n
-        }
-    }
-    return 0
-}
