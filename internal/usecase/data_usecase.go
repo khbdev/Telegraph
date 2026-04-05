b808671ff4c6f285f121aeb724bffa29aa5967dc
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 16:29:34 +0500

    Auto commit: 2026-04-05 16:29:34

--- internal/usecase/data_usecase.go
+++ internal/usecase/data_usecase.go
@@ -24,7 +24,7 @@ func NewDataUsecase(dataRepo domain.DataRepository, urlRepo domain.URLRepository
 
 type CreateDataOutput struct {
 	Data *models.Data
-	URL  *models.URL.
+	URL  *model
 }
 
 // CreateData usecase
