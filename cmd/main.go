9a728b23930733d8c5cb2243f67b62f9c2205ac4
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 14:55:41 +0500

    Auto commit: 2026-04-05 14:55:41

--- cmd/main.go
+++ cmd/main.go
@@ -42,5 +42,7 @@ func main(){
    }
 ctx, candel := context.WithTimeout(context.Background(), 3 * time.Second)
 defer candel()
-   dataRepo.Create(ctx, *data)
+   dataRepo.Create(ctx, &data)
+
+   
 }
