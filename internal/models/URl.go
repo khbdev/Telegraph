b27ea665fb05796f7128aa52a685c28c446e9d5d
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 14:27:23 +0500

    Auto commit: 2026-04-05 14:27:23

--- internal/models/URl.go
+++ internal/models/URl.go
@@ -6,6 +6,5 @@ import "gorm.io/gorm"
 type URL struct {
 	gorm.Model
 	Title     string `gorm:"type:varchar(255);not null"`
-	YourName  string `gorm:"type:varchar(255);not null"`
-	YourStory string `gorm:"type:text;not null"`
+	
 }
