799271508a2618115a088674c9acfc5c4ce243a0
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 14:42:06 +0500

    Auto commit: 2026-04-05 14:42:06

--- internal/domain/repostory_data.go
+++ internal/domain/repostory_data.go
@@ -7,5 +7,5 @@ import (
 
 type DataRepository interface {
 	Create(ctx context.Context, data *models.Data) (*models.Data, error)
-	Update(ctx context.Context, data *models.Data) (*models.Data, error)
+	Update(ctx context.Context, data *models.Data) ( error)
 }
