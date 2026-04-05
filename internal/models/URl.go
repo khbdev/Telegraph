95866147604264b640dcee7a44fe9d4fe6a14d91
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 16:06:00 +0500

    Auto commit: 2026-04-05 16:06:00

--- internal/models/URl.go
+++ internal/models/URl.go
@@ -2,10 +2,9 @@ package models
 
 import "gorm.io/gorm"
 
-
 type URL struct {
 	gorm.Model
-	URL    string `gorm:"type:varchar(255);not null"`
+	URL    string `gorm:"type:varchar(255);not null;index"` 
 
 	DataID uint  
 	Data   Data   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
--- pkg/numberGeneration/generate.go
+++ pkg/numberGeneration/generate.go
@@ -1,2 +1,14 @@
 package numbergeneration
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
+func AddRandomNumberPrefix(title string) string {
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(9000) + 1000 
+	return fmt.Sprintf("%s-%d", title, num)
+}
