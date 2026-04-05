5cbaaf145681b195ce38582e62a688bd2812fb70
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 14:33:11 +0500

    Auto commit: 2026-04-05 14:33:11

--- internal/models/URl.go
+++ internal/models/URl.go
@@ -5,5 +5,8 @@ import "gorm.io/gorm"
 
 type URL struct {
 	gorm.Model
-	URL string `gorm:"type:varchar(255);not null"`
+	URL    string `gorm:"type:varchar(255);not null"`
+
+	DataID uint   // foreign key
+	Data   Data   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
