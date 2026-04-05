5996bb777aaed2a8f71f6f44476f15b568f18742
Azizbek <webcoderazizbek@gmail.com>
2026-04-05 14:41:53 +0500

    Auto commit: 2026-04-05 14:41:53

--- internal/domain/repostory_data.go
+++ internal/domain/repostory_data.go
@@ -1,1 +1,11 @@
 package domain
+
+import (
+	"context"
+	"your_project/internal/models"
+)
+
+type DataRepository interface {
+	Create(ctx context.Context, data *models.Data) (*models.Data, error)
+	Update(ctx context.Context, data *models.Data) (*models.Data, error)
+}
