38f3e0097de99f47e112b8e936c0b53ccc477aa9
Azizbek <webcoderazizbek@gmail.com>
2026-04-06 19:22:47 +0500

    Auto commit: 2026-04-06 19:22:47

---
+++ pkg/response/response_json.go
@@ -0,0 +1,13 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type SuccessResponse struct {
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data"`
+}
+
+type ErrorResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+}
