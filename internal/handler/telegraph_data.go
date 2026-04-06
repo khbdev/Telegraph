57255156a60ee21fe1231396093b5e2f04f056e5
Azizbek <webcoderazizbek@gmail.com>
2026-04-06 19:23:28 +0500

    Auto commit: 2026-04-06 19:23:28

--- internal/handler/telegraph_data.go
+++ internal/handler/telegraph_data.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"telegraph-clone/internal/models"
+	"telegraph-clone/internal/usecase"
+)
+
+type DataHandler struct {
+	uc *usecase.DataUsecase
+}
+
+func NewDataHandler(uc *usecase.DataUsecase) *DataHandler {
+	return &DataHandler{
+		uc: uc,
+	}
+}
