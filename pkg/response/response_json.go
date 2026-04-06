a3c4e8482e536b433efb65cb13eb504a8b7a1891
Azizbek <webcoderazizbek@gmail.com>
2026-04-06 19:23:10 +0500

    Auto commit: 2026-04-06 19:23:10

--- pkg/response/response_json.go
+++ pkg/response/response_json.go
@@ -11,3 +11,18 @@ type ErrorResponse struct {
 	Success bool   `json:"success"`
 	Error   string `json:"error"`
 }
+
+
+func JSONSuccess(c *gin.Context, status int, data interface{}) {
+	c.JSON(status, SuccessResponse{
+		Success: true,
+		Data:    data,
+	})
+}
+
+func JSONError(c *gin.Context, status int, message string) {
+	c.JSON(status, ErrorResponse{
+		Success: false,
+		Error:   message,
+	})
+}
