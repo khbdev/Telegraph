54d267703bc8124131dadce793a1c5ffff040208
Azizbek <webcoderazizbek@gmail.com>
2026-04-06 19:39:13 +0500

    Auto commit: 2026-04-06 19:39:13

--- pkg/numberGeneration/generate.go
+++ pkg/numberGeneration/generate.go
@@ -9,6 +9,15 @@ import (
 
 func AddRandomNumberPrefix(title string) string {
 	rand.Seed(time.Now().UnixNano())
-	num := rand.Intn(9000) + 1000 
-	return fmt.Sprintf("%s-%d", title, num)
+
+	// 1. lower case
+	slug := strings.ToLower(title)
+
+	// 2. bo‘sh joylarni "-" ga almashtirish
+	slug = strings.ReplaceAll(slug, " ", "-")
+
+	// 3. random number
+	num := rand.Intn(9000) + 1000
+
+	return fmt.Sprintf("/%s-%d", slug, num)
 }
