72b11efbbe1e6d803c639f23288b25987e6d9031
Azizbek <webcoderazizbek@gmail.com>
2026-04-06 19:23:44 +0500

    Auto commit: 2026-04-06 19:23:44

--- internal/handler/telegraph_data.go
+++ internal/handler/telegraph_data.go
@@ -18,3 +18,28 @@ func NewDataHandler(uc *usecase.DataUsecase) *DataHandler {
 	}
 }
 
+
+
+func (h *DataHandler) CreateData(c *gin.Context) {
+	var input models.CreateDataInput
+
+	// 1. JSON parse
+	if err := c.ShouldBindJSON(&input); err != nil {
+		JSONError(c, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	// 2. usecase chaqiramiz
+	result, err := h.uc.CreateData(c.Request.Context(), input)
+	if err != nil {
+		// ❗ tizim xatoni chiqarmaymiz
+		JSONError(c, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+
+	// 3. response qaytaramiz
+	JSONSuccess(c, http.StatusCreated, gin.H{
+		"url":  result.URL,
+		"data": result.Data,
+	})
+}
